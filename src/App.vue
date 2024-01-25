b5035d8c1e8c6a3248854ff697bb5f4d977939cd
ThiagoWaldrich <thiagowlf7@gmail.com>
2024-01-24 22:00:04 -0300

    header up

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
-<script setup lang="ts">
+<script>
+import Banner from "./components/Banner.vue";
 
+export default {
+  components: { Banner: Banner }
+}
 </script>
-
 <template>
-<h1>Meu primeiro Vue !</h1>
+  <Banner />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<Banner/>
+<style scoped></style>
